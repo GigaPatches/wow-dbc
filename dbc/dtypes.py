15937c3cbabf5ed0f5caf3f15ad2325137a3b8b9
Justin <justin@justin-laptop.(none)>
2010-05-17 19:28:34 -0400

    Initial commit

---
+++ dbc/dtypes.py
@@ -0,0 +1,24 @@
+"""
+"""
+
+class Base(object):
+    def __init__(self, name):
+        self.name = name
+
+class Int32(Base):
+    c = 'i'
+
+class UInt32(Base):
+    c = 'I'
+
+class Int64(Base):
+    c = 'q'
+
+class UInt64(Base):
+    c = 'Q'
+
+class Float(Base):
+    c = 'f'
+
+class String(Base):
+    c = 'I'
---
+++ test.py
@@ -0,0 +1,14 @@
+from dbc import DBCFile
+from dbc.dtypes import UInt32, String, Int32
+
+class ChatProfanityDBC(DBCFile):
+    skeleton = [
+        UInt32('id'),
+        String('word'),
+        Int32('lang'), #TODO: Implement Enum?
+    ]
+
+words = ChatProfanityDBC('ChatProfanity.dbc').read()
+for word in words:
+    if word['lang'] in (-1, 0):
+        print word['word']
